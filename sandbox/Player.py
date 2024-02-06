29c59d22336994b46a2cd036e18baeafade76c17
DerfDa180155 <derflinger.david@gmail.com>
2024-02-06 17:51:42 +0100

    Player movement limit with ray casting

--- sandbox/Player.py
+++ sandbox/Player.py
@@ -9,7 +9,7 @@ class Player:
         self.x = x
         self.y = y
         self.direction = direction
-        self.speed = 300
+        self.maxSpeed = 150
         self.scaleWidth = 1600
         self.scaleHeight = 900
         self.scaleSizeWidth = 1600
@@ -17,7 +17,7 @@ class Player:
         self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
         self.frontRaysViewAngle = 90
-        self.frontRaysDeg = 45
+        self.frontRaysDeg = 22.5
         self.frontRays = []
 
         i = self.frontRaysViewAngle / (-2)
@@ -27,13 +27,20 @@ class Player:
 
     def move(self, moveDir: int):
         if moveDir == 0:
-            # update coordinates based on direction and speed
-            self.x += (self.speed / 100) * np.cos(np.deg2rad(self.direction))
-            self.y += (self.speed / 100) * np.sin(np.deg2rad(self.direction))
+            checkMove = True
+
+            for ray in self.frontRays:
+                if ray.length <= 10:
+                    checkMove = False
+
+            if checkMove:
+                # update coordinates based on direction and speed
+                self.x += (self.maxSpeed / 100) * np.cos(np.deg2rad(self.direction))
+                self.y += (self.maxSpeed / 100) * np.sin(np.deg2rad(self.direction))
         elif moveDir == 1:
             # update coordinates based on direction and speed
-            self.x -= (self.speed / 100) * np.cos(np.deg2rad(self.direction))
-            self.y -= (self.speed / 100) * np.sin(np.deg2rad(self.direction))
+            self.x -= (self.maxSpeed / 200) * np.cos(np.deg2rad(self.direction))
+            self.y -= (self.maxSpeed / 200) * np.sin(np.deg2rad(self.direction))
 
     def changeDir(self, direction: int):
         self.direction += direction
