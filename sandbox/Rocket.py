b8de4c95b0c1d72cd23c9ac5014bc22707987c0b
DerfDa180155 <derflinger.david@gmail.com>
2024-06-07 23:50:20 +0200

    added rocket explosion effect

--- sandbox/Rocket.py
+++ sandbox/Rocket.py
@@ -19,12 +19,18 @@ class Rocket:
         self.rays = [Ray.Ray(self.x, self.y, self.direction)]
 
         self.living = True
+        self.explode = False
+        self.explodeRadius = 0
 
     def draw(self, surface):
         newX = (self.x * surface.get_width()) / 1600
         newY = (self.y * surface.get_height()) / 900
-        size = (20 * surface.get_width()) / 2000
-        pygame.draw.rect(surface, (70, 150, 70), pygame.Rect(newX - (size / 2), newY - (size / 2), size, size))
+        if not self.explode:
+            size = (20 * surface.get_width()) / 2000
+            pygame.draw.rect(surface, (70, 150, 70), pygame.Rect(newX - (size / 2), newY - (size / 2), size, size))
+        else:
+            scaledRadius = ((self.explodeRadius + 10) * surface.get_width()) / 2000
+            pygame.draw.circle(surface, (200, 150, 10), (newX, newY), scaledRadius, 0)
 
     def updateRays(self, bounds: array):
         for ray in self.rays:
@@ -33,11 +39,16 @@ class Rocket:
     def update(self):
         for ray in self.rays:
             if ray.length <= 10 and ray.length >= 0:
+                self.explode = True
+
+        if not self.explode:
+            self.x += (self.speed / 100) * np.cos(np.deg2rad(self.direction))
+            self.y += (self.speed / 100) * np.sin(np.deg2rad(self.direction))
+
+            for ray in self.rays:
+                ray.updateRay(self.x, self.y, self.direction)
+
+        else:
+            self.explodeRadius += 0.5
+            if self.explodeRadius >= 20:
                 self.living = False
-
-        self.x += (self.speed / 100) * np.cos(np.deg2rad(self.direction))
-        self.y += (self.speed / 100) * np.sin(np.deg2rad(self.direction))
-
-        for ray in self.rays:
-            ray.updateRay(self.x, self.y, self.direction)
-
