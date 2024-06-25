c7553e9939ede7d611921cf924409f60dedb8048
DerfDa180155 <derflinger.david@gmail.com>
2024-06-25 23:45:44 +0200

    added rainbow color cycle effect to god mode item

--- sandbox/GodMode.py
+++ sandbox/GodMode.py
@@ -15,6 +15,9 @@ class GodMode:
         self.maxLiveTime = 120 * 10 # 10 seconds
         self.liveTime = self.maxLiveTime
         self.living = True
+        self.color = (255, 128, 0)
+        self.colorIncreaser = [False, False, False]
+        self.colorSpeed = 3
 
         self.summonedPlayer.godMode = True
         self.summonedPlayer.currentMaxSpeed = self.summonedPlayer.maxSpeed + 70
@@ -25,7 +28,25 @@ class GodMode:
         playerSizeWidth = (20 * surface.get_width()) / self.summonedPlayer.scaleSizeWidth
         playerSizeHeight = (20 * surface.get_height()) / self.summonedPlayer.scaleSizeHeight
 
-        pygame.draw.circle(surface, (200, 150, 10), (x, y), playerSizeWidth, 0)
+        pygame.draw.circle(surface, self.color, (x, y), playerSizeWidth, 0)
+
+        colorArray = [self.color[0], self.color[1], self.color[2]]
+
+        for i in range(len(colorArray)):
+            if self.colorIncreaser[i]:
+                colorArray[i] += self.colorSpeed
+                if colorArray[i] > 255:
+                    colorArray[i] = 255 - self.colorSpeed
+                    self.colorIncreaser[i] = False
+            else:
+                colorArray[i] -= self.colorSpeed
+                if colorArray[i] < 0:
+                    colorArray[i] = self.colorSpeed
+                    self.colorIncreaser[i] = True
+
+
+        self.color = (colorArray[0], colorArray[1], colorArray[2])
+        print(self.color)
 
     def update(self):
         if self.liveTime <= 0:
